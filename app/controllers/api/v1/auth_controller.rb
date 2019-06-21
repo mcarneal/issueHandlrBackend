ba368d5d94335ee6a9d216be79bcf91567aec12a
github username <github email address>
2019-06-20 20:55:52 -0400

    auth controller changed raise: false

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuthController < ApplicationController
 
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create], raise: false
 
   def create
     @employee = Employee.find_by(username: employee_login_params[:username])
